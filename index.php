47e9bec03f96a6ba20321c1e2aae513be39d08f7
Ben Stephenson <ben@undesk.co>
2015-01-22 14:41:15 +0000

    Super simple routing to a controller action

--- index.php
+++ index.php
@@ -3,3 +3,16 @@ define('ROOT', __DIR__);
 include 'src/autoload.php';
 include 'vendor/autoload.php';
 
+$HomeController = new HomeController();
+$allowableActions = ['calculate' => 'HomeController:calculate', 'home' => 'HomeController:index'];
+$actionRequested = $_GET['action'];
+
+if (!$actionRequested || !isset($allowableActions[$actionRequested])) {
+  $actionRequested = 'home';
+}
+
+$arr = explode(':', $allowableActions[$actionRequested]);
+
+// Render the "route"
+$$arr[0]->{ $arr[1] }();
+
